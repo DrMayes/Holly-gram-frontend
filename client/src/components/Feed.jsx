5561c47bf9f8c3a79886d0f452a29a5334aa0453
Brandon Chan <bchan26@bu.edu>
2022-11-30 11:34:17 -0500

    completed boilerplate for home and feed, routes are commented out but ready for when we have more components. next step is to create boilerplate for sign-in and register

---
+++ client/src/components/Feed.jsx
@@ -0,0 +1,16 @@
+export default function Feed () {
+    return (
+    <div is='FeedContent'>
+
+        <div className="FeedPosts">
+        <h1>Hello World!</h1>
+        </div>
+
+        <div className="FeedPosts">
+        <h1>We'll set this up so it maps out content</h1>
+        </div>
+
+
+
+    </div>
+    )}
